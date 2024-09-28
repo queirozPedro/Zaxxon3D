9f09320193828c0f72dbbab21e840f6f7fa2c267
queirozPedro <pedrovinic12@gmail.com>
2024-09-28 15:42:44 -0300

    Colocando o modelo da nave. Ainda sem movimentação.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Cena, câmera e renderizador
 const scene = new THREE.Scene();
@@ -11,6 +12,7 @@ document.body.appendChild(renderer.domElement);
 camera.position.set(8, 10, 10);
 camera.lookAt(0, 0, 0);
 
+
 // Controles orbitais para teste
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -20,39 +22,78 @@ controls.update();
 // Classe Player que representa o jogador
 class Player {
     constructor() {
-        // Declaração do player (nave do jogador)
-        const playerGeometry = new THREE.BoxGeometry(0.5, 0.5, 1);
-        const playerMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const player = new Player(new THREE.Mesh(playerGeometry, playerMaterial));
-        scene.add(player.player);
-        this.player = player;
+
+        this.model = null
+        this.load(this)
+
         this.minHeight = 0.35; // Altura mínima (limite do chão)
         this.maxHeight = 5;   // Altura máxima (limite superior)
-        //definir a altura inicial 
-        this.player.position.y = this.minHeight;
+        
+        this.maxWidthVariation = 9.65;   // Limites de movimentação lateral
+        
+        // definir a altura inicial 
+        // this.position.y = this.minHeight;
+    }
+
+    load(object){
+        // Instantiate a loader
+        const loader = new GLTFLoader();
+        
+        // Load a glTF resource
+        loader.load(
+            // resource URL
+            '/assets/models/scene.gltf',
+            // called when the resource is loaded
+            function ( gltf ) {
+        
+                scene.add( gltf.scene );
+                object.model = gltf.scene.children[0];
+        
+                gltf.animations; // Array<THREE.AnimationClip>
+                gltf.scene; // THREE.Group
+                gltf.scenes; // Array<THREE.Group>
+                gltf.cameras; // Array<THREE.Camera>
+                gltf.asset; // Object
+        
+            },
+            // called while loading is progressing
+            function ( xhr ) {
+        
+                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        
+            },
+            // called when loading has errors
+            function ( error ) {
+        
+                console.log( 'An error happened' );
+        
+            }
+        );
     }
 
     movementControls(event) {
         switch (event.key) {
             case 'ArrowUp':
             case 'w':
-                if (this.player.position.y < this.maxHeight) {
-                    this.player.position.y += 0.1;
+                if (this.position.y < this.maxHeight) {
+                    this.position.y += 0.1;
                 }
                 break;
             case 'ArrowDown':
             case 's':
-                if (this.player.position.y > this.minHeight) {
-                    this.player.position.y -= 0.1;
+                if (this.position.y > this.minHeight) {
+                    this.position.y -= 0.1;
                 }
                 break;
             case 'ArrowLeft':
             case 'a':
-                this.player.position.x -= 0.1;
+                if(this.position.x > -this.maxWidthVariation)
+                    this.position.x -= 0.1;
                 break;
             case 'ArrowRight':
             case 'd':
-                this.player.position.x += 0.1;
+                if(this.position.x < this.maxWidthVariation)
+                    this.position.x += 0.1;
                 break;
         }
     }
@@ -67,6 +108,7 @@ const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2;
 plane.position.z = -20;
 scene.add(plane);
+
 
 // Função animate, que executa a cada quadro
 function animate() {
